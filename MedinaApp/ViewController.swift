f8d3bbb291f610be5fc92505e8237cc43873b454
Abdullah Aloufi <Abdullah.aloufi@hotmail.com>
2018-02-05 05:02:50 +0300

    Initial Commit

---
+++ MedinaApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MedinaApp
+//
+//  Created by Abdullah Aloufi on 19/05/1439 AH.
+//  Copyright © 1439 Abdullah Aloufi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
